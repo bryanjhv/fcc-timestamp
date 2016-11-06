fc2683864b32b61002a980b561f953fd7281c434
Bryan Horna <bryanjhv@gmail.com>
2016-11-06 21:51:04 +0000

    Add server logic

---
+++ server.js
@@ -0,0 +1,32 @@
+var app = require("express")();
+var moment = require("moment");
+
+var format = "MMMM DD, YYYY";
+
+
+function listener(req, res) {
+  var timestamp = req.params.timestamp;
+  var result = {unix: null, natural: null};
+
+  if (timestamp) {
+    var date;
+
+    if (isNaN(Number(timestamp))) {
+      // Date string
+      date = moment(timestamp, format, "en", true);
+    } else {
+      // UNIX timestamp
+      date = moment.unix(timestamp * 1000);
+    }
+
+    if (date.isValid()) {
+      result.unix = date.unix();
+      result.natural = date.format(format);
+    }
+  }
+
+  res.json(result);
+}
+
+
+app.get("/:timestamp?", listener).listen(process.env.PORT);
